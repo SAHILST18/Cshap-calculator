d30f611a739a786d0e8ad579b82f06cf2c188f7d
sahil s trambadia <sahiltrambadia18@gmail.com>
2021-10-11 10:23:24 +0530

    Create calculator.cs

---
+++ calculator.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the action to be performed");
+            Console.WriteLine("Press 1 for Addition");
+            Console.WriteLine("Press 2 for Subtraction");
+            Console.WriteLine("Press 3 for Multiplication");
+            Console.WriteLine("Press 4 for Division \n");
+            double action = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter 1st input");
+            double input_1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter 2nd input");
+            double input_2 = Convert.ToDouble(Console.ReadLine());
+            double result = 0;
+            switch (action)
+            {
+                case 1:
+                    {
+                        result = Addition(input_1, input_2);
+                        break;
+                    }
+                case 2:
+                    {
+                        result = Subtraction(input_1, input_2);
+                        break;
+                    }
+                case 3:
+                    {
+                        result = Multiplication(input_1, input_2);
+                        break;
+                    }
+                case 4:
+                    {
+                        result = Division(input_1, input_2);
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Wrong action!! try again");
+                    break;
+            }
+            Console.WriteLine("The result is {0}", result);
+            Console.ReadKey();
+        }
+        //Addition  
+        public static double Addition(double input_1, double input_2)
+        {
+            double result = input_1 + input_2;
+            return result;
+        }
+        //Substraction  
+        public static double Subtraction(double input_1, double input_2)
+        {
+            double result = input_1 - input_2;
+            return result;
+        }
+        //Multiplication  
+        public static double Multiplication(double input_1, double input_2)
+        {
+            double result = input_1 * input_2;
+            return result;
+        }
+        //Division  
+        public static double Division(double input_1, double input_2)
+        {
+            double result = input_1 / input_2;
+            return result;
+        }
+    }
+}
